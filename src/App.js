f655b46d011936ac408a875b567caa7fce4411f2
James Zagorski <james_zagorski@yahoo.com>
2022-11-22 00:15:34 -0700

    HTML Added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello!
-      </header>
+   
+<div>
+    <div>
+        <h3>DocPro Powered by Valor</h3>
     </div>
+    <div>
+        <div>
+            <h3>Number of Providers</h3>
+            <h3>Average Patients Seen per Day</h3>
+        </div>
+    </div>
+    <div>
+        <div>
+            <h3>Insurance Mix</h3>
+                <div>Medicaid</div>
+                <div>Commercial</div>
+                <div>Medicare</div>
+        </div>
+    </div>
+    <div> 
+        <div>
+            <h3>What Services do you currently offer? And at what percent are you capturing your eligible population?</h3>
+                <div>Ankle Brachial Index</div>
+                <div>Depression Screen</div>
+                <div>Alcohol Screen</div>
+                <div>Cardio Screen</div>
+                <div>Obesity Counseling</div>
+                <div>Allergy Testing</div>
+                <input type="text"/>
+                <button>Submit</button>
+        </div>
+    </div>
+</div>
   );
 }
 
